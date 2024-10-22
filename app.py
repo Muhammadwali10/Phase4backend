a3071aa7279d998189cc22d728b4e9b81aaa4564
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-10-22 08:14:11 +0300

    complete lab

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 db.init_app(app)
 jwt = JWTManager(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/*": {"origins": "https://phase4frontend.onrender.com"}})
 
 with app.app_context():
     db.create_all()
